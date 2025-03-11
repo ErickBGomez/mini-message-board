2f07e5cc5a52530d4d88ef4ec6db8f612c1a9f9d
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2025-03-11 16:26:22 -0600

    Show messages in index

--- routes/messagesRouter.js
+++ routes/messagesRouter.js
@@ -1,8 +1,12 @@
 const { Router } = require("express");
+const {
+  getIndex,
+  getNewMessage,
+} = require("../controllers/messagesController");
 
 const messagesRouter = Router();
 
-// messagesRouter.get("/", index here);
+messagesRouter.get("/", getIndex);
 // messagesRouter.get("/new", message form here);
 
 module.exports = messagesRouter;
